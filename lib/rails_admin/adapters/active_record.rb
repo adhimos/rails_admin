bb7ce822f53616023132a7db6473161cdcdb426f
David Chriqui <david@Davids-MacBook-Pro-2.local>
2019-02-27 14:18:23 +0100

    fix activerecord adapter for hotels table

--- lib/rails_admin/adapters/active_record.rb
+++ lib/rails_admin/adapters/active_record.rb
@@ -3,6 +3,8 @@ require 'rails_admin/adapters/active_record/abstract_object'
 require 'rails_admin/adapters/active_record/association'
 require 'rails_admin/adapters/active_record/property'
 
+@@current_table = 'NO_TABLE'
+
 module RailsAdmin
   module Adapters
     module ActiveRecord
@@ -106,6 +108,7 @@ module RailsAdmin
             @values << value1 unless value1.nil?
             @values << value2 unless value2.nil?
             table, column = column_infos[:column].split('.')
+            @@current_table = table
             @tables.push(table) if column
           end
         end
@@ -222,33 +225,47 @@ module RailsAdmin
 
         def build_statement_for_string_or_text
           return if @value.blank?
-
-          return ["(#{@column} = ?)", @value] if ['is', '='].include?(@operator)
+          return ["(#{@column} = ?)", @value] if ['is', '='].include?(@operator) || @column.include?('.id')
 
           unless ['postgresql', 'postgis'].include? ar_adapter
             @value = @value.mb_chars.downcase
           end
 
-          @value = begin
-            case @operator
-            when 'default', 'like'
-              "#{@value}%"
-            when 'starts_with'
-              "#{@value}%"
-            when 'ends_with'
-              "%#{@value}"
+          if ['hotels'].include? @@current_table
+            @value = begin
+              case @operator
+              when 'default', 'like'
+                "#{@value.sub(/^[0-9]+/, '').gsub(/[0-9]+/, '%')}%".gsub(/%{2,}/, '%')
+              when 'starts_with'
+                "#{@value}%"
+              when 'ends_with'
+                "%#{@value}"
+              else
+                return
+              end
+            end
+
+            ["(LOWER(#{@column}) LIKE LOWER(?))", @value]
+          else
+            @value = begin
+              case @operator
+              when 'default', 'like'
+                "%#{@value}%"
+              when 'starts_with'
+                "#{@value}%"
+              when 'ends_with'
+                "%#{@value}"
+              else
+                return
+              end
+            end
+
+            if ['postgresql', 'postgis'].include? ar_adapter
+              ["(#{@column} ILIKE ?)", @value]
             else
-              return
+              ["(LOWER(#{@column}) LIKE ?)", @value]
             end
           end
-
-          # if ['postgresql', 'postgis'].include? ar_adapter
-          #   ["(#{@column} ILIKE ?)", @value]
-          # else
-          #   ["(LOWER(#{@column}) LIKE ?)", @value]
-          # end
-
-          ["(LOWER(#{@column}) LIKE LOWER(?))", @value]
         end
 
         def build_statement_for_enum
